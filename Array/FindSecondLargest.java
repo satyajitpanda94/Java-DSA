92be0e166bc3f943f379371e581c28579c777459
satyajitpanda94 <saatjit94@gmail.com>
2026-09-21 20:23:05 +0530

    java-DSA - Added FindSecondLargest By Stream api from Array

--- Array/FindSecondLargest.java
+++ Array/FindSecondLargest.java
@@ -1,9 +1,25 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class FindSecondLargest {
     public static void main(String[] args) {
          int[] arr = { 45, 78, 10, 31, 65, 94, 48 };
         int largest1 = findSecondLargest(arr);
         System.out.println(largest1);
+        int largest2 = findSecondLargestByStream(arr);
+        System.out.println(largest2);
     }
+
+	private static int findSecondLargestByStream(int[] arr) {
+		return Arrays.stream(arr)
+        // .boxed()
+        // .mapToObj(value->Integer.valueOf(value))
+        .mapToObj(Integer::valueOf)
+        .sorted((a,b)->b-a)
+        .skip(1)
+        .findFirst()
+        .orElse(0);
+	}
 
 	private static int findSecondLargest(int[] arr) {
 		int largest=0;
