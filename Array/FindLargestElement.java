6e4f007313e8d1f9f6b1f617205caf9d4ea611e7
satyajitpanda94 <saatjit94@gmail.com>
2026-09-21 18:13:30 +0530

    java-DSA - Added findLargest element in array by stream api

--- Array/FindLargestElement.java
+++ Array/FindLargestElement.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
+
 public class FindLargestElement{
-    public static int findLargestElement(int[] arr){
+    public static int findLargest(int[] arr){
         int largest=arr[0];
 
         for (int i : arr) {
@@ -10,9 +12,16 @@ public class FindLargestElement{
 
         return  largest;
     }
+
+    public static int findLargestByStream(int[] arr){
+        return Arrays.stream(arr).max().getAsInt();
+    }
+
     public static void main(String[] args) {
         int[] arr={45,78,10,31,65,94,48};
-        int largest=findLargestElement(arr);
-        System.out.println(largest);
+        int largest1=findLargest(arr);
+        int largest2=findLargestByStream(arr);
+        System.out.println(largest1);
+        System.out.println(largest2);
     }
 }
