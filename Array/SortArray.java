28d3086f1af3d1ebb01e4baa645658586bfa781f
satyajitpanda94 <saatjit94@gmail.com>
2026-09-23 20:27:39 +0530

    Added Sorted Array by stream api

--- Array/SortArray.java
+++ Array/SortArray.java
@@ -2,18 +2,28 @@ import java.util.Arrays;
 
 public class SortArray {
     public static void main(String[] args) {
-        int arr[]={14,78,25,49,36,57,24,12};
+        int arr[] = { 14, 78, 25, 49, 36, 57, 24, 12 };
 
         System.out.println(Arrays.toString(sortedArray(arr)));
+
+        int arr2[] = { 14, 78, 25, 49, 36, 57, 24, 12, 20 };
+
+        System.out.println(Arrays.toString(sortedArrayByStream(arr2)));
     }
 
-    public static int[] sortedArray(int[] arr){
+    private static int[] sortedArrayByStream(int[] arr) {
+        return Arrays.stream(arr)
+                .sorted()
+                .toArray();
+    }
+
+    public static int[] sortedArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                if(arr[i]<arr[j]){
-                    int temp=arr[j];
-                    arr[j]=arr[i];
-                    arr[i]=temp;
+                if (arr[i] < arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
                 }
             }
         }
