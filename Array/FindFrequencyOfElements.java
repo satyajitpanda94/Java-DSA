5679599e02e3b9b606b11483be783e853f5648d5
satyajitpanda94 <saatjit94@gmail.com>
2026-09-24 21:31:11 +0530

    Added FindFrequencyOfElemts

--- Array/FindFrequencyOfElements.java
+++ Array/FindFrequencyOfElements.java
@@ -3,14 +3,14 @@ import java.util.stream.Collectors;
 
 public class FindFrequencyOfElements {
     public static void main(String[] args) {
-        int[] arr={25,78,13,48,25,64,75,25,13,78,78,23};
+        int[] arr = { 25, 78, 13, 48, 25, 64, 75, 25, 13, 78, 78, 23 };
 
         System.err.println(findFrequency(arr));
     }
 
     private static Map<Integer, Long> findFrequency(int[] arr) {
         return Arrays.stream(arr)
-        .boxed()
-        .collect(Collectors.groupingBy(n->n, Collectors.counting()));
+                .boxed()
+                .collect(Collectors.groupingBy(n -> n, Collectors.counting()));
     }
 }
