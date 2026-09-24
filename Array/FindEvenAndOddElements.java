6819a3ec6e12fa46352588528fe48ae7f54d1dbd
satyajitpanda94 <saatjit94@gmail.com>
2026-09-24 21:12:53 +0530

    Added FindEvenAndOddElements

---
+++ Array/FindEvenAndOddElements.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindEvenAndOddElements {
+    public static Map<String, Integer> findEvenAndOdd(int[] arr) {
+        Map<String, Integer> evenAndOdd = new HashMap<>();
+        int oddCount = 0;
+        int evenCount = 0;
+
+        for (int i : arr) {
+            if (i % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        evenAndOdd.put("evenCount", evenCount);
+        evenAndOdd.put("oddCount", oddCount);
+        return evenAndOdd;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 24, 15, 7, 13, 71, 55, 20, 94, 36, 47 };
+
+        System.out.println(findEvenAndOdd(arr));
+    }
+}
