011ce4852b80ea4c311444d4e5e19ef5c1b3daec
satyajitpanda94 <saatjit94@gmail.com>
2026-09-26 00:03:10 +0530

    Added FindMissingNumber

--- Array/FindMissingNumber.java
+++ Array/FindMissingNumber.java
@@ -1,15 +1,16 @@
 public class FindMissingNumber {
     public static void main(String[] args) {
-        int[] input={1,2,3,5,6};
+        int[] input = { 1, 2, 3, 4, 5 };
         System.out.println(findMissingNumber(input, 6));
     }
 
     private static int findMissingNumber(int[] input, int n) {
-        int j = 0;
-        for (int i = 0; i < n; i++) {
-            if(input[i] != ++j){
+        int j = 1;
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] != j) {
                 return j;
             }
+            j++;
         }
 
         return j;
